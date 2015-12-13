0870b71c6fc99e490314add94d780bbdd1e7622a
qq273999947 <15229234589@163.com>
2015-12-13 19:25:35 +0800

    写时拷贝

--- String-COW.cpp
+++ String-COW.cpp
@@ -30,6 +30,20 @@ public:
 	{
 		Release(_str);
 	}
+	char&operator[](size_t pos)
+	{
+		if(--GetRefCount(_str) != 0)
+		{
+			char *tmp = new char[strlen(_str) + 5];	
+			--GetRefCount(_str);
+			tmp += 4;
+			strcpy(tmp,_str);
+			_str = tmp;
+			tmp = NULL;
+			GetRefCount(_str) = 1;
+		}
+		return _str[pos];
+	}
 private:
 	void Release(char *p)
 	{
@@ -46,15 +60,22 @@ private:
 };
 void Test1()
 {
-	String s1("11111");
+	String s1("111111111111111111111");
 	String s2(s1);
 
-	String s3("22222");
+	String s3("222222222222222222222");
 	s1 =s1;
+}
+void Test2()
+{
+	String s1("11211111111111111111");
+	cout<<s1[2]<<endl;
+
 }
 int main()
 {
-	Test1();
+	//Test1();
+	Test2();
 	system("pause");
 	return 0;
 }
